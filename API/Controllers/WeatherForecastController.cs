0980a3689d436765890a173824089b01faff0cfc
statzt <60333777+statzt@users.noreply.github.com>
2020-11-22 14:50:36 -0600

    Completed activity 9

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -1,100 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using Application.Posts;
-using Domain;
-using MediatR;
-using Microsoft.AspNetCore.Mvc;
 using System.Linq;
-using Persistence;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
-        public class PostsController : ControllerBase
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
     {
-        private readonly IMediator mediator;
-        public PostsController(IMediator mediator) => this.mediator = mediator; 
-
-        public async Task<ActionResult<List<Post>>> List() 
+        private static readonly string[] Summaries = new[]
         {
-            return await this.mediator.Send(new List.Query());
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
         }
-        /// <summary>
-        /// GET api/posts
-        /// </summary>
-        /// <returns>A list of posts</returns>
+
         [HttpGet]
-        public ActionResult<List<Post>> Get()
+        public IEnumerable<WeatherForecast> Get()
         {
-            return this.context.Posts.ToList();
-
-        }
-
-        /// <summary>
-        /// Get api/posts/[id]
-        /// </summary>
-        /// <param name="id">Post id</param>
-        /// <returns>A single post</returns>
-        [HttpGet("{id}")]
-        public ActionResult<Post> GetById(Guid id)
-        {
-            return this.context.Posts.Find(id);
-        }
-         /// <summary>
-        /// POST api/post
-        /// </summary>
-        /// <param name="request">JSON request containing post fields</param>
-        /// <returns>A new post</returns>
-        [HttpPost]
-        public ActionResult<Post> Create([FromBody]Post request)
-        {
-            var post = new Post
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Id = request.Id,
-                Title = request.Title,
-                Body = request.Body,
-                Date = request.Date,
-            };
-
-            context.Posts.Add(post);
-            var success = context.SaveChanges() > 0;
-
-            if (success)
-            {
-                return post;
-            }
-
-            throw new Exception("Error creating post");
-        }
-        /// <summary>
-        /// PUT api/put
-        /// </summary>
-        /// <param name="request">JSON request containing one or more updated post fields</param>
-        /// <returns>An update post</returns>
-        [HttpPut]
-        public ActionResult<Post> Update([FromBody]Post request)
-        {
-            var post = context.Posts.Find(request.Id);
-
-            if (post == null)
-            {
-                throw new Exception("Could not find post");
-            }
-
-            post.Title = request.Title != null ? request.Title : post.Title;
-            post.Body = request.Body != null ? request.Body : post.Body;
-            post.Date = request.Date != null ? request.Date : post.Date;
-
-            var success = context.SaveChanges() > 0;
-
-            if (success)
-            {
-                return post;
-            }
-
-            throw new Exception("Error updating post");
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
         }
     }
 }
